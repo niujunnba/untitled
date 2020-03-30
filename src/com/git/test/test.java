47e3500867bb9d4f632760f40b5d1714751fb3ab
unknown <214076966@qq.com>
2020-03-30 16:44:21 +0800

    Initial commit

---
+++ src/com/git/test/test.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+/**
+ * @Classname test
+ * @Description TODO
+ * @Date 2020/3/30 16:38
+ * @Created by admin
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
